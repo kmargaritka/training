83a4e78d880a95f411c66ee4f82e3957abffdbc2
Marharyta Kurkul <Marharyta_Kurkul@epam.com>
2017-02-06 15:30:14 +0300

    Fixed Training 6

--- sites/all/modules/custom/training6_i18n/js/training6_i18n.js
+++ sites/all/modules/custom/training6_i18n/js/training6_i18n.js
@@ -1,19 +1,8 @@
-/**
- * training6_i18n behavior to table.
- *
- * @type {Drupal~behavior}
- *
- * @prop {Drupal~behaviorAttach} attach
- */
-;(function($){
+(function($){
   Drupal.behaviors.training6i18nDate = {
-    attach : function (context_date) {
+    attach : function () {
       console.log(Drupal.t(Drupal.settings.registration_date));
       console.log(Drupal.t(Drupal.settings.days_till_new_year));
-    }
-  };
-  Drupal.behaviors.training6i18nTextarea = {
-    attach : function (context_textarea) {
       $('#button').click(function (e) {
         var textarea = $('#textarea')[0];
         textarea.value = Drupal.checkPlain(textarea.value);
