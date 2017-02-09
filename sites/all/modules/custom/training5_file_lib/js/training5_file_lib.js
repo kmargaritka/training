35f3d2785061d5fc41110e47956a6aeb93d25001
Marharyta Kurkul <Marharyta_Kurkul@epam.com>
2017-02-09 09:50:50 +0300

    Fixed

--- sites/all/modules/custom/training5_file_lib/js/training5_file_lib.js
+++ sites/all/modules/custom/training5_file_lib/js/training5_file_lib.js
@@ -2,7 +2,7 @@
 
   Drupal.behaviors.training5FileLibTable = {
     attach : function (context, settings) {
-      var records = settings.training5_file_lib;
+      var records = Drupal.settings.training5_file_lib;
       $.each(records, function(){
         var $selector = $('.uid-' + this + ' td');
         console.log($selector);
